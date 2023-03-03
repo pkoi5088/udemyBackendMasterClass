74d249021ed14c91cdd37916201ed79f2a3fa1a0
Seoki <pkoi5088@gmail.com>
2023-03-04 00:19:41 +0900

    user sqlc add

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -13,11 +13,13 @@ type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Accounts, error)
 	CreateEntry(ctx context.Context, arg CreateEntryParams) (Entries, error)
 	CreateTransfer(ctx context.Context, arg CreateTransferParams) (Transfers, error)
+	CreateUser(ctx context.Context, arg CreateUserParams) (Users, error)
 	DeleteAccount(ctx context.Context, id int64) error
 	GetAccount(ctx context.Context, id int64) (Accounts, error)
 	GetAccountForUpdate(ctx context.Context, id int64) (Accounts, error)
 	GetEntry(ctx context.Context, id int64) (Entries, error)
 	GetTransfer(ctx context.Context, id int64) (Transfers, error)
+	GetUser(ctx context.Context, username string) (Users, error)
 	ListAccounts(ctx context.Context, arg ListAccountsParams) ([]Accounts, error)
 	ListEntry(ctx context.Context, arg ListEntryParams) ([]Entries, error)
 	ListTransfer(ctx context.Context, arg ListTransferParams) ([]Transfers, error)
